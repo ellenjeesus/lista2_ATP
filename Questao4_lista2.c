ad456425aa103ce81949e2fa2082caf7ca71fefa
ellenjeesus <ellenjvet@gmail.com>
2026-09-02 12:03:39 -0400

    Questao 4 lista 2

---
+++ Questao4_lista2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // Declaracao de variaveis
+    int idade;
+    
+    // Entrada de dados
+    printf("Digite sua idade: ");
+    scanf("%d",&idade);
+    
+    // Processamento
+    if(idade>=18){
+      printf("Voce e maior de idade\n");
+ }else{
+      printf("Voce nao e maior de idade\n");
+ } 
+    
+    
+return 0;
+}            
